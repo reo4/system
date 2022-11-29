9749d51ab7e47bcdb6682fa8ddd8e4d47e29fc49
Ahmed Yasser <ahmedreo4@gmail.com>
2022-11-29 11:00:48 +0200

    edit views path

--- libs/edge.js
+++ libs/edge.js
@@ -3,15 +3,16 @@ const { join } = require('path')
 
 const edge = new Edge({ cache: false })
 
-edge.mount(join(__dirname, 'views'))
+// edge.mount(join(__dirname, 'views'))
 
 const registerEdge = app => {
   app.engine('edge', (filePath, options, callback) => {
+    console.log(filePath)
     const rendered = edge.renderSync(filePath, options)
     return callback(null, rendered)
   })
 
-  app.set('views', join(__dirname, 'views'))
+  app.set('views', join(__dirname, '../', 'views'))
 
   app.set("view engine", "edge");
 }
